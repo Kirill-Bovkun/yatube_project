f9104ef21f54e747e629aff4bd5b0371647d81f8
Kirill Bovkun <mr.bovkun.k@gmail.com>
2022-09-15 22:54:55 +0700

    Change function group_posts in views

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
 
 def index(request):
     template = 'posts/index.html'
     return render(request, template)
 
 
-def group_posts(request, slug):
-    return HttpResponse(f'Группы: {slug}')
+def group_posts(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
