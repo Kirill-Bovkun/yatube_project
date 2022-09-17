c207b99d82df753db7feeb36723d48d2dc30309e
Kirill Bovkun <mr.bovkun.k@gmail.com>
2022-09-17 22:03:13 +0700

    Change urlpaterns (add namespace and name in path), add dictionary (context) in views.py

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path('', views.index),
-               path('group/<slug:slug>/', views.group_posts),
+app_name = 'posts'
+
+urlpatterns = [path('', views.index, name='posts'),
+               path('group/', views.group_posts, name='groups'),
                ]
