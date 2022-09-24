c263a87b61a973a3af6523c1fc769fb5c520598a
Kirill Bovkun <mr.bovkun.k@gmail.com>
2022-09-24 13:58:42 +0700

    Work version, add class Group and many things

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'posts'
 
 urlpatterns = [path('', views.index, name='posts'),
-               path('group/', views.group_posts, name='groups'),
+               path('group/<slug:slug>/', views.group_posts, name='group'),
                ]
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
+    path('group/', include('posts.urls', namespace='group')),
     path('admin/', admin.site.urls)
                ]
